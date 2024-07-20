169d9c5e5071f3d008ab579021d6447d286bf03e
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 20:49:21 +0800

    [Feat] builder sidebar components.

---
+++ components/builder/SideBarBtnElement.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BuilderElement } from "./BuilderElements";
+import { Button } from "../ui/button";
+import { useDraggable } from "@dnd-kit/core";
+import { cn } from "@/lib/utils";
+
+function SideBarBtnElement({
+  builderElement,
+}: {
+  builderElement: BuilderElement;
+}) {
+  const { label, icon: Icon } = builderElement.designerBtnElement;
+
+  const draggable = useDraggable({
+    id: `designer-btn-${builderElement.type}`,
+    data: {
+      type: builderElement.type,
+      isDesignerBtnElement: true,
+    },
+  });
+
+  return (
+    <Button
+      ref={draggable.setNodeRef}
+      variant={"outline"}
+      className={cn(
+        "flex flex-col gap-2 size-[120px]",
+        draggable.isDragging && "ring-2 ring-primary"
+      )}
+      {...draggable.listeners}
+      {...draggable.attributes}
+    >
+      <Icon className="size-8 text-primary cursor-grab" />
+      <p className="text-xs">{label}</p>
+    </Button>
+  );
+}
+
+export function SideBarBtnElementDragOverlay({
+  builderElement,
+}: {
+  builderElement: BuilderElement;
+}) {
+  const { label, icon: Icon } = builderElement.designerBtnElement;
+
+  return (
+    <Button
+      variant={"outline"}
+      className="flex flex-col gap-2 size-[120px] cursor-grab"
+    >
+      <Icon className="size-8 text-primary cursor-grab" />
+      <p className="text-xs">{label}</p>
+    </Button>
+  );
+}
+
+export default SideBarBtnElement;
