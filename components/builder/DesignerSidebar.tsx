6798cb916534ff683a16e841698b0b4f83644ecf
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 20:57:49 +0800

    [Style] Added Padding.

--- components/builder/DesignerSidebar.tsx
+++ components/builder/DesignerSidebar.tsx
@@ -4,7 +4,7 @@ import { BuilderElements } from "./BuilderElements";
 
 function DesignerSidebar() {
   return (
-    <aside className="w-[600px] max-w-[600px] flex flex-col flex-grow h-full bg-background">
+    <aside className="w-[600px] max-w-[600px] flex flex-col flex-grow h-full bg-background px-5">
       <SideBarBtnElement builderElement={BuilderElements.TextField} />
     </aside>
   );
