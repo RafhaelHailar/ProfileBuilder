fe0eb4c8bb7e5f49a2fecaa2da72b5eda53884e6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 17:35:27 +0800

    [Feat] text field component.

---
+++ components/builder/field/TextField.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { ElementsType, BuilderElement } from "../BuilderElements";
+import { MdTextFields } from "react-icons/md";
+
+const type: ElementsType = "TextField";
+
+export const TextFieldBuilderElement: BuilderElement = {
+  type,
+  construct: (id: string) => ({
+    id,
+    type,
+    extraAttributes: {
+      label: "Text Field",
+      helperText: "Helper Text",
+      required: false,
+      placeHolder: "Type here...",
+    },
+  }),
+  designerBtnElement: {
+    icon: <MdTextFields />,
+    label: "Text Field",
+  },
+  designerComponent: () => <div>Designer Component</div>,
+  builderComponent: () => <div>Builder Component</div>,
+  propertiesComponent: () => <div>Properties Component</div>,
+};
