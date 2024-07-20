4ab21f144854c3fc8565a4ceb25a2e0156ecab77
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 17:29:50 +0800

    [Refactor] move to builder folder

--- components/builder/Designer.tsx
+++ components/builder/Designer.tsx
@@ -14,8 +14,8 @@ function Designer() {
     <div className="flex w-full h-full">
       <DesignerSidebar />
       <div className="p-4 w-full">
-        <div className="bg-background m-auto w-full h-full max-w-[920px] rounded-xl flex flex-col flex-grow items-center justify-start flex-1">
-          <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+        <div className="bg-background m-auto w-full h-full max-w-[320px] rounded-xl flex flex-col flex-grow items-center justify-start flex-1">
+          <p className="text-2xl text-muted-foreground flex flex-grow items-center font-bold">
             Drop here
           </p>
         </div>
