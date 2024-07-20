812df677430f760512d4b9ba294a254d6db37542
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 21:03:10 +0800

    [Feat] show dropping area.

--- components/builder/Designer.tsx
+++ components/builder/Designer.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 import DesignerSidebar from "./DesignerSidebar";
 import { useDroppable } from "@dnd-kit/core";
+import { cn } from "@/lib/utils";
+import { BuilderElementInstance } from "./BuilderElements";
 
 function Designer() {
   const droppable = useDroppable({
@@ -14,10 +15,23 @@ function Designer() {
     <div className="flex w-full h-full">
       <DesignerSidebar />
       <div className="p-4 w-full">
-        <div className="bg-background m-auto w-full h-full max-w-[320px] rounded-xl flex flex-col flex-grow items-center justify-start flex-1">
-          <p className="text-2xl text-muted-foreground flex flex-grow items-center font-bold">
-            Drop here
-          </p>
+        <div
+          ref={droppable.setNodeRef}
+          className={cn(
+            "bg-background m-auto w-full h-full max-w-[320px] rounded-xl flex flex-col flex-grow items-center justify-start flex-1",
+            droppable.isOver && "ring-2 ring-primary/20"
+          )}
+        >
+          {!droppable.isOver && (
+            <p className="text-2xl text-muted-foreground flex flex-grow items-center font-bold">
+              Drop here
+            </p>
+          )}
+          {droppable.isOver && (
+            <div className="p-4 w-full">
+              <div className="h-[40px] rounded-md bg-primary/20"></div>
+            </div>
+          )}
         </div>
       </div>
     </div>
