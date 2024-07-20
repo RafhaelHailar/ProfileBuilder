97d7b6c2fa37b9776b39d407d8eda4a7ff32a616
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-20 17:30:27 +0800

    [Feat] builder elements.

---
+++ components/builder/BuilderElements.tsx
@@ -0,0 +1,32 @@
+import { TextFieldBuilderElement } from "./field/TextField";
+
+export type ElementsType = "TextField";
+
+export interface BuilderElement {
+  type: ElementsType;
+
+  construct: (id: string) => BuilderElementInstance;
+
+  designerBtnElement: {
+    icon: React.ReactElement;
+    label: string;
+  };
+
+  designerComponent: React.FC;
+  builderComponent: React.FC;
+  propertiesComponent: React.FC;
+}
+
+export interface BuilderElementInstance {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+}
+
+type BuilderElementsType = {
+  [key in ElementsType]: BuilderElement;
+};
+
+export const BuilderElements: BuilderElementsType = {
+  TextField: TextFieldBuilderElement,
+};
